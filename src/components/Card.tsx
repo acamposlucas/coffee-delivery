cd3081ae43502236bd5eafe6b3e9f952b875daac
lucas <lac.lucas96@gmail.com>
2022-10-31 20:13:53 -0300

    fix: card display on large screens

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,199 +3,219 @@ import { ShoppingCart, Trash } from "phosphor-react";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import { CounterButton } from "./Buttons/CounterButton";
 interface Coffee {
-  id: number;
-  name: string;
-  description: string;
-  tags: string[];
-  image: string;
-  price: number;
+	id: number;
+	name: string;
+	description: string;
+	tags: string[];
+	image: string;
+	price: number;
 }
 interface CardProps extends Coffee {}
 
 export const Card = ({
-  id,
-  image,
-  name,
-  description,
-  tags,
-  price,
+	id,
+	image,
+	name,
+	description,
+	tags,
+	price,
 }: CardProps) => {
-  const { getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart } = useShoppingCart();
+	const {
+		getItemQuantity,
+		increaseCartQuantity,
+		decreaseCartQuantity,
+		removeFromCart,
+	} = useShoppingCart();
 
-  const quantity = getItemQuantity(id);
+	const quantity = getItemQuantity(id);
 
-  return (
-    <CardContainer>
-      <img src={image} alt={name} />
-      <div className="container">
-        <div className="tags">
-          {tags.map((tag) => (
-            <span key={tag} className="tag" aria-hidden="true">{tag}</span>
-          ))}
-        </div>
-        <div>
-          <h3>{name}</h3>
-          <p>{description}</p>
-        </div>
-        <div className="inner-container">
-          <strong>
-            <span>R$</span>
-            {price.toFixed(2)}
-          </strong>
-          <div className="actions">
-            {
-              quantity > 0 ? (
-                <>
-                  <IconButton type="button" onClick={() => removeFromCart(id)}>
-                    <span className="visually-hidden">Remover do carrinho</span>
-                    <Trash size={22} color="#ffffff" weight="fill"/>
-                  </IconButton>
-                  <CounterButton quantity={quantity} id={id} />
-                </>
-              ) : (
-                <IconButton type="button" onClick={() => increaseCartQuantity(id)}>
-                  <span className="visually-hidden">Adicionar no carrinho</span>
-                  <ShoppingCart size={22} color="#ffffff" weight="fill" />
-                </IconButton>
-              )
-            }
-          </div>
-        </div>
-      </div>
-    </CardContainer>
-  );
+	return (
+		<CardContainer>
+			<img src={image} alt={name} />
+			<div className="container">
+				<div className="tags">
+					{tags.map((tag) => (
+						<span key={tag} className="tag" aria-hidden="true">
+							{tag}
+						</span>
+					))}
+				</div>
+				<div>
+					<h3>{name}</h3>
+					<p>{description}</p>
+				</div>
+				<div className="inner-container">
+					<strong>
+						<span>R$</span>
+						{price.toFixed(2)}
+					</strong>
+					<div className="actions">
+						{quantity > 0 ? (
+							<>
+								<IconButton
+									type="button"
+									onClick={() => removeFromCart(id)}>
+									<span className="visually-hidden">
+										Remover do carrinho
+									</span>
+									<Trash
+										size={22}
+										color="#ffffff"
+										weight="fill"
+									/>
+								</IconButton>
+								<CounterButton quantity={quantity} id={id} />
+							</>
+						) : (
+							<IconButton
+								type="button"
+								onClick={() => increaseCartQuantity(id)}>
+								<span className="visually-hidden">
+									Adicionar no carrinho
+								</span>
+								<ShoppingCart
+									size={22}
+									color="#ffffff"
+									weight="fill"
+								/>
+							</IconButton>
+						)}
+					</div>
+				</div>
+			</div>
+		</CardContainer>
+	);
 };
 
 const CardContainer = styled.li`
-  background-color: ${(props) => props.theme.colors["gray-200"]};
-  border-top-right-radius: 32px;
-  border-bottom-left-radius: 32px;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  padding-block: 1.25rem;
-  padding-inline: 1.25rem;
-  position: relative;
-  height: 310px;
-  width: 100%;
-  max-width: 368px;
+	background-color: ${(props) => props.theme.colors["gray-200"]};
+	border-top-right-radius: 32px;
+	border-bottom-left-radius: 32px;
+	padding-block: 1.25rem;
+	padding-inline: 1.25rem;
+	position: relative;
+	height: 100%;
+	width: 100%;
+	max-width: 20rem;
 
-  img {
-    aspect-ratio: 1;
-    position: absolute;
-    width: 120px;
-    inset-inline: 50%;
-    inset-block: -20px;
-    transform: translateX(-50%);
-  }
+	img {
+		aspect-ratio: 1;
+		width: 128px;
+		position: relative;
+		display: block;
+		margin-inline: auto;
+		margin-block-start: -56px;
+		margin-block-end: 16px;
+	}
 
-  .container {
-    align-items: center;
-    display: flex;
-    flex-direction: column;
-    text-align: center;
+	.container {
+		align-items: center;
+		display: flex;
+		flex-direction: column;
+		text-align: center;
 
-    .tags {
-      align-items: center;
-      display: flex;
-      justify-content: center;
-      gap: 0.5rem;
-      margin-block-end: 1rem;
+		.tags {
+			align-items: center;
+			display: flex;
+			justify-content: center;
+			gap: 0.5rem;
+			margin-block-end: 1rem;
 
-      span.tag {
-        background-color: ${(props) => props.theme.colors["yellow-100"]};
-        border-radius: 100px;
-        color: ${(props) => props.theme.colors["yellow-700"]};
-        font-size: ${(props) => props.theme.fontSize.xs};
-        font-weight: ${(props) => props.theme.fontWeight.bold};
-        padding-block: 4px;
-        padding-inline: 8px;
-        text-transform: uppercase;
-      }
-    }
+			span.tag {
+				background-color: ${(props) =>
+					props.theme.colors["yellow-100"]};
+				border-radius: 100px;
+				color: ${(props) => props.theme.colors["yellow-700"]};
+				font-size: ${(props) => props.theme.fontSize.xs};
+				font-weight: ${(props) => props.theme.fontWeight.bold};
+				padding-block: 4px;
+				padding-inline: 8px;
+				text-transform: uppercase;
+			}
+		}
 
-    h3 {
-      color: ${(props) => props.theme.colors["black-800"]};
-      font-family: ${(props) => props.theme.fontFamily.baloo};
-      font-size: ${(props) => props.theme.fontSize.xl};
-      margin-block-end: 0.5rem;
-    }
+		h3 {
+			color: ${(props) => props.theme.colors["black-800"]};
+			font-family: ${(props) => props.theme.fontFamily.baloo};
+			font-size: ${(props) => props.theme.fontSize.xl};
+			margin-block-end: 0.5rem;
+		}
 
-    p {
-      color: ${(props) => props.theme.colors["gray-700"]};
-      font-size: ${(props) => props.theme.fontSize.sm};
-    }
+		p {
+			color: ${(props) => props.theme.colors["gray-700"]};
+			font-size: ${(props) => props.theme.fontSize.sm};
+		}
 
-    .inner-container {
-      align-items: end;
-      display: flex;
-      gap: 1rem;
-      justify-content: space-between;
-      margin-block-start: 1rem;
-      width: 100%;
+		.inner-container {
+			align-items: end;
+			display: flex;
+			gap: 1rem;
+			justify-content: space-between;
+			margin-block-start: 1rem;
+			width: 100%;
 
-      .actions {
-        align-items: center;
-        display: flex;
-        gap: 0.5rem;
-        justify-content: space-between;
+			.actions {
+				align-items: center;
+				display: flex;
+				gap: 0.5rem;
+				justify-content: space-between;
 
-        .counter {
-          align-items: center;
-          background-color: ${(props) => props.theme.colors["gray-400"]};
-          border-radius: 6px;
-          display: flex;
-          gap: 0.5rem;
-          padding-block: 0.5rem;
-          padding-inline: 0.5rem;
-        }
-      }
-    }
-  }
+				.counter {
+					align-items: center;
+					background-color: ${(props) =>
+						props.theme.colors["gray-400"]};
+					border-radius: 6px;
+					display: flex;
+					gap: 0.5rem;
+					padding-block: 0.5rem;
+					padding-inline: 0.5rem;
+				}
+			}
+		}
+	}
 `;
 
 export const ButtonContainer = styled.div`
-  align-items: center;
-  background-color: ${(props) => props.theme.colors["gray-400"]};
-  border-radius: 6px;
-  color: ${(props) => props.theme.colors["black-900"]};
-  display: flex;
-  font-size: ${(props) => props.theme.fontSize.xs};
-  gap: 0.5rem;
-  justify-content: center;
-  padding-inline: 0.75rem;
-  height: 40px;
-  line-height: 40px;
-  text-transform: uppercase;
+	align-items: center;
+	background-color: ${(props) => props.theme.colors["gray-400"]};
+	border-radius: 6px;
+	color: ${(props) => props.theme.colors["black-900"]};
+	display: flex;
+	font-size: ${(props) => props.theme.fontSize.xs};
+	gap: 0.5rem;
+	justify-content: center;
+	padding-inline: 0.75rem;
+	height: 40px;
+	line-height: 40px;
+	text-transform: uppercase;
 
-  button {
-    align-items: center;
-    border: 0;
-    display: flex;
-    justify-content: center;
+	button {
+		align-items: center;
+		border: 0;
+		display: flex;
+		justify-content: center;
 
-    svg {
-      &:hover {
-        fill: red;
-      }
-    }
-  }
+		svg {
+			&:hover {
+				fill: red;
+			}
+		}
+	}
 `;
 
 export const IconButton = styled.button`
-  align-items: center;
-  background-color: ${(props) => props.theme.colors["purple-900"]};
-  border: 0;
-  border-radius: 6px;
-  display: flex;
-  justify-content: center;
-  height: 40px;
-  line-height: 40px;
-  padding-inline: 0.5rem;
-  transition: background-color 0.3s ease-in;
+	align-items: center;
+	background-color: ${(props) => props.theme.colors["purple-900"]};
+	border: 0;
+	border-radius: 6px;
+	display: flex;
+	justify-content: center;
+	height: 40px;
+	line-height: 40px;
+	padding-inline: 0.5rem;
+	transition: background-color 0.3s ease-in;
 
-  &:hover {
-    background-color: ${(props) => props.theme.colors["purple-500"]};
-  }
+	&:hover {
+		background-color: ${(props) => props.theme.colors["purple-500"]};
+	}
 `;
-
--- src/pages/Index/style.ts
+++ src/pages/Index/style.ts
@@ -3,131 +3,129 @@ import { IconContainer } from "../../styles/components/IconContainer";
 import { Heading } from "../../styles/components/Typography";
 
 export const Container = styled.section`
-  display: flex;
-  gap: 3.5rem;
-  grid-template-columns: repeat(2, minmax(0, 1fr));
-  padding-block: 6rem;
-  width: 90%;
-  margin-inline: auto;
+	display: flex;
+	gap: 3.5rem;
+	grid-template-columns: repeat(2, minmax(0, 1fr));
+	padding-block: 6rem;
+	width: 90%;
+	margin-inline: auto;
 
-  @media (max-width: 768px) {
-    flex-direction: column;
-  }
+	@media (max-width: 768px) {
+		flex-direction: column;
+	}
 
-  .hero {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    gap: 4rem;
+	.hero {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		gap: 4rem;
 
-    @media (max-width: 768px) {
-      order: 2;
-    }
+		@media (max-width: 768px) {
+			order: 2;
+		}
 
-    .hero-container {
-      ${Heading} {
-        /* color: ${(props) => props.theme.colors["black-900"]};
+		.hero-container {
+			${Heading} {
+				/* color: ${(props) => props.theme.colors["black-900"]};
         font-family: ${(props) => props.theme.fontFamily.baloo};
         font-size: ${(props) => props.theme.fontSize["5xl"]};
         font-weight: ${(props) => props.theme.fontWeight.extraBold}; */
-        margin-block-end: 1rem;
-      }
+				margin-block-end: 1rem;
+			}
 
-      p {
-        color: ${(props) => props.theme.colors["black-800"]};
-        font-size: ${(props) => props.theme.fontSize.xl};
-      }
-    }
+			p {
+				color: ${(props) => props.theme.colors["black-800"]};
+				font-size: ${(props) => props.theme.fontSize.xl};
+			}
+		}
 
-    .hero-list {
-      display: grid;
-      grid-template-columns: repeat(2, auto);
-      gap: 1.25rem;
-      list-style-type: none;
-      width: 100%;
+		.hero-list {
+			display: grid;
+			grid-template-columns: repeat(2, auto);
+			gap: 1.25rem;
+			list-style-type: none;
+			width: 100%;
 
-      @media (max-width: 1024px) {
-        grid-template-columns: repeat(1, 1fr);
-      }
+			@media (max-width: 1024px) {
+				grid-template-columns: repeat(1, 1fr);
+			}
 
-      li {
-        align-items: center;
-        color: ${(props) => props.theme.colors["black-700"]};
-        display: flex;
+			li {
+				align-items: center;
+				color: ${(props) => props.theme.colors["black-700"]};
+				display: flex;
 
-        ${IconContainer} {
-          margin-inline-end: 0.75rem;
-        }
+				${IconContainer} {
+					margin-inline-end: 0.75rem;
+				}
 
-        &:nth-child(1) {
-          ${IconContainer} {
-            background-color: ${(props) => props.theme.colors["yellow-700"]};
-          }
-        }
+				&:nth-child(1) {
+					${IconContainer} {
+						background-color: ${(props) =>
+							props.theme.colors["yellow-700"]};
+					}
+				}
 
-        &:nth-child(2) {
-          ${IconContainer} {
-            background-color: ${(props) => props.theme.colors["yellow-400"]};
-          }
-        }
+				&:nth-child(2) {
+					${IconContainer} {
+						background-color: ${(props) =>
+							props.theme.colors["yellow-400"]};
+					}
+				}
 
-        &:nth-child(3) {
-          ${IconContainer} {
-            background-color: ${(props) => props.theme.colors["black-700"]};
-          }
-        }
+				&:nth-child(3) {
+					${IconContainer} {
+						background-color: ${(props) =>
+							props.theme.colors["black-700"]};
+					}
+				}
 
-        &:nth-child(4) {
-          ${IconContainer} {
-            background-color: ${(props) => props.theme.colors["purple-900"]};
-          }
-        }
-      }
-    }
-  }
+				&:nth-child(4) {
+					${IconContainer} {
+						background-color: ${(props) =>
+							props.theme.colors["purple-900"]};
+					}
+				}
+			}
+		}
+	}
 
-  img {
-    width: 100%;
-    height: 100%;
-    @media (max-width: 768px) {
-      order: 1;
-    }
-  }
+	img {
+		width: 100%;
+		height: 100%;
+		@media (max-width: 768px) {
+			order: 1;
+		}
+	}
 `;
 
 export const MenuContainer = styled.section`
-  padding-block: 2rem;
-  width: 90%;
-  margin-inline: auto;
+	padding-block: 2rem;
+	width: 90%;
+	margin-inline: auto;
 
-  h2 {
-    font-family: ${(props) => props.theme.fontFamily.baloo};
-    font-weight: ${(props) => props.theme.fontWeight.extraBold};
-    font-size: ${(props) => props.theme.fontSize["3xl"]};
-    color: ${(props) => props.theme.colors["black-800"]};
-    padding-block-end: 2rem;
-  }
+	h2 {
+		font-family: ${(props) => props.theme.fontFamily.baloo};
+		font-weight: ${(props) => props.theme.fontWeight.extraBold};
+		font-size: ${(props) => props.theme.fontSize["3xl"]};
+		color: ${(props) => props.theme.colors["black-800"]};
+		padding-block-end: 2rem;
+	}
 
-  ul {
-    display: grid;
-    column-gap: 2rem;
-    grid-template-columns: repeat(1, 1fr);
-    justify-content: center;
-    place-items: center;
-    row-gap: 2.5rem;
-    list-style-type: none;
-    margin-inline: auto;
+	ul {
+		list-style-type: none;
+		margin-inline: auto;
+		display: grid;
+		row-gap: 2rem;
+		column-gap: 1rem;
+		place-items: center;
 
-    @media (min-width: 768px) {
-      grid-template-columns: repeat(2, 1fr);
-    }
+		@media (min-width: 768px) {
+			grid-template-columns: repeat(2, minmax(0, 1fr));
+		}
 
-    @media (min-width: 1024px) {
-      grid-template-columns: repeat(3, 3fr);
-    }
-
-    @media (min-width: 1440px) {
-      grid-template-columns: repeat(4, 1fr);
-    }
-  }
+		@media (min-width: 1024px) {
+			grid-template-columns: repeat(3, minmax(0, 1fr));
+		}
+	}
 `;
